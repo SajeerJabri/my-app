15a6d0f077cdfa5edba1ff2edb33643cb3fc1812
Sajeer Jabri <sajeerjabri883@gmail.com>
2020-06-21 03:19:14 +0500

    Revert "delete some useless file and code"
    This reverts commit 84fe782a91e500e9c7af48448e5af71ce29aac4d.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div>
-     <h1>Hello World all of us.</h1>
+     <h1>Hello World</h1>
      </div>
   );
 }
