7fe3e0fd57c4da80a79767b2cb9e28a7d8773d7f
Sajeer Jabri <sajeerjabri883@gmail.com>
2020-06-21 03:58:49 +0500

    add paragraph in App.js File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-function App(props) {
+const App=() => {
   return (
     <div>
      <h1>Hello World all of us.</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut est quae ea perferendis iste a fuga adipisci. Labore illo 
+     ullam suscipit veritatis dignissimos sit, omnis vitae, expedita, officiis est quam!</p>
      </div>
   );
 }
