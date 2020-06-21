eb1469459fa1b610421b904204a30ff9458e2bed
Sajeer Jabri <sajeerjabri883@gmail.com>
2020-06-21 05:20:41 +0500

    heading update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App=() => {
   return (
     <div>
-     <h1>Hello World all of us.</h1>
+     <h1>welcome to my website.</h1>
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut est quae ea perferendis iste a fuga adipisci. Labore illo 
      ullam suscipit veritatis dignissimos sit, omnis vitae, expedita, officiis est quam!</p>
      </div>
