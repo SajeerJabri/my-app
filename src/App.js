a17ae207b6f3e5f08981fd5425bd7f4294146f4c
Sajeer Jabri <sajeerjabri883@gmail.com>
2020-06-21 18:36:30 +0500

    add button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App=() => {
      <h1>welcome to my website.</h1>
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut est quae ea perferendis iste a fuga adipisci. Labore illo 
      ullam suscipit veritatis dignissimos sit, omnis vitae, expedita, officiis est quam!</p>
+     <button style={{padding: "10px 25px"}}>Click Me</button>
      </div>
   );
 }
